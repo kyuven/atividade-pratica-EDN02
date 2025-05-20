d029df33b3a63feb1a40f23d32c49e4f8f9588f5
lucca <101908843+kyuven@users.noreply.github.com>
2025-05-20 20:06:21 -0300

    Update atividade1.py

--- atividade1.py
+++ atividade1.py
@@ -1,62 +1,24 @@
-def numeros():
+def registro():
+    notas = []
+    print("Digite as notas dos alunos e digite 'fim' para encerrar:")
     while True:
-        try:
-            num1 = float(input('Insira o primeiro número: '))
+        nota = input("Nota: ")
+        if nota.lower() == "fim":
             break
-        except ValueError:
-            print('Entrada inválida, por favor digite um número válido!')
-
-    while True:
         try:
-            num2 = float(input('Insira o segundo número: '))
-            break
+            nota_float = float(nota)
+            if 0 <= nota_float <= 10:
+                notas.append(nota_float)
+            else:
+                print("Digite uma nota válida entre 0 e 10.")
         except ValueError:
-            print('Entrada inválida, por favor digite um número válido!')
+            print("Entrada inválida. Por favor, digite um número ou 'fim'.")
 
-    return num1, num2
+    if notas:
+        media = sum(notas) / len(notas)
+        print("A média da turma é:", round(media, 2))
+    else:
+        print("Nenhuma nota foi registrada.")
 
-def operadores(num1, num2):
-    while True:
-        print('''Qual operação deseja realizar?
-        1. + (adição)
-        2. - (subtração)
-        3. * (multiplicação)
-        4. / (divisão)
-        0. Sair da calculadora''')
-
-        try:
-            operador = int(input('Escolha uma opção: '))
-        except ValueError:
-            print('Entrada inválida, por favor insira um número de 0 a 4.')
-            continue
-
-        if operador == 0:
-            print('Saindo da Calculadora!')
-            return None
-
-        if operador == 1:
-            return num1 + num2
-        elif operador == 2:
-            return num1 - num2
-        elif operador == 3:
-            return num1 * num2
-        elif operador == 4:
-            try:
-                return num1 / num2
-            except ZeroDivisionError:
-                print('Erro: Não é possível dividir por zero. Tente novamente.')
-        else:
-            print('Operação inválida. Tente novamente.')
-            
-        
-def calculadora():
-    num1, num2 = numeros()
-    resultado = operadores(num1, num2)
-    if resultado != None:
-        print('O resultado de sua conta é: ', resultado)
-
-
-print('''Bem vindo a Calculadora!
-Será pedido que insira dois números separadamente, após deverá escolher um das operações: + (adição), - (subtração), * (multiplicação) e / (divisão).''')
-
-calculadora()
+print("Bem-vindo ao Registro de Notas! Calcule a médiada sua turma com praticidade")
+registro()
